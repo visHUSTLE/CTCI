cf9a272528718472a97d0964d086b745c4fd6ab4
visHUSTLE <33931832+visHUSTLE@users.noreply.github.com>
2020-04-02 21:39:27 -0400

    LC_Update

--- Leetcode_30Day_Challenge/happyNumber.py
+++ Leetcode_30Day_Challenge/happyNumber.py
@@ -18,3 +18,30 @@ def isHappy(n):
 	return True
 
 print(isHappy(19))
+
+
+#Approach 2
+#Space - O(1)
+
+def numSquare(n):
+	sum = 0
+	while n:
+		sum += (n % 10) * (n % 10)
+		n //= 10
+	return sum
+
+def isHappy_1(n):
+	slow = n
+	fast = n
+	while True:
+		slow = numSquare(slow)
+		fast = numSquare(numSquare(fast))
+
+		if (slow != fast):
+			continue
+		else:
+			break
+			
+	return slow == 1
+
+print(isHappy_1(82))
